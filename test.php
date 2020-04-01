6a5a2cb47f0f2564d825d0090f4dabf30d1f7a62
LauraColeman <57405611+LauraColeman@users.noreply.github.com>
2020-04-01 11:42:44 +0100

    Add files via upload
    testing email server

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC ="-//W3C//DTD XHTML 1.0 Transitional//EN" "http://w3.org/TR/xhtml1/DTD/xhtml-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Testing Mail</title>
+</head>
+<body>
+
+<?php
+
+mail("l.coleman99@outlook.com","testing","test of mail function","From: test@hexample.co.uk")
+
+?>
+
+
+</body>
+</html>
+
+
+
